c5de4401570c37ed94369934897100916a9bc895
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2025-06-13 19:32:49 +0200

    bug fix

---
+++ src/partials/ReviewsVideo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { PlyrLayout, plyrLayoutIcons } from '@vidstack/react/player/layouts/plyr';
+
+// styles
+
+const ReviewsVideo = ({ video_reviews }: { video_reviews: any }) => {
+  return (
+    <div className="reviews-wrapper flex gap-2.5 md:gap-3.5 lg:gap-5">
+      {video_reviews.video_urls.map((item: any, index: number) => (
+        <div key={index} className="reviews-slide flex-[0_0_320px] rounded-xl overflow-hidden">
+          <MediaPlayer className="size-full" title="Sprite Fight" src={item.video_url} poster={item.poster} aspectRatio="16 / 9">
+            <MediaProvider />
+            <PlyrLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={plyrLayoutIcons} />
+          </MediaPlayer>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ReviewsVideo;
