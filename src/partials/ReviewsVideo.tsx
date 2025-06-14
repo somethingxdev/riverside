59fad52ac4d315e1edfce6c1c6ee5afac259650d
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2025-06-14 10:44:10 +0200

    bug fix

--- src/partials/ReviewsVideo.tsx
+++ src/partials/ReviewsVideo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react';
 import { PlyrLayout, plyrLayoutIcons } from '@vidstack/react/player/layouts/plyr';
 
 // styles
@@ -9,8 +9,10 @@ const ReviewsVideo = ({ video_reviews }: { video_reviews: any }) => {
     <div className="reviews-wrapper flex gap-2.5 md:gap-3.5 lg:gap-5">
       {video_reviews.video_urls.map((item: any, index: number) => (
         <div key={index} className="reviews-slide flex-[0_0_320px] rounded-xl overflow-hidden">
-          <MediaPlayer className="size-full" title="Sprite Fight" src={item.video_url} poster={item.poster} aspectRatio="16 / 9">
-            <MediaProvider />
+          <MediaPlayer className="size-full" src={item.video_url} poster={item.poster} aspectRatio="16 / 9">
+            <MediaProvider>
+              <Poster src={item.poster} alt="Video Review" />
+            </MediaProvider>
             <PlyrLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={plyrLayoutIcons} />
           </MediaPlayer>
         </div>
